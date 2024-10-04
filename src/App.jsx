fff57f937b22b783fecf2740dd155f6feb20f74b
Panos Tolios <ptolios@deloitte.gr>
2024-10-04 17:14:32 +0300

    Add routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Layout from "./layouts/Layout"
+import HomePage from "./pages/Home"
+import AboutPage from "./pages/About"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+    ],
+  },
+])
+
 function App() {
   return (
     <>
-      <Header />
-      <main className="container mx-auto">
-      </main>
-      <Footer />
+      <RouterProvider router={router} />
     </>
   )
 }
