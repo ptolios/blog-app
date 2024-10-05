7829b4b4f0b7f11cd7cc2a7ffb0f8aff4a46d94d
Panos Tolios <ptolios@deloitte.gr>
2024-10-06 00:40:57 +0300

    Setup active NavLinks

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,7 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,41 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Header() {
   return (
-    <>
-      <header className="container mx-auto h-40 py-4 flex flex-row bg-white justify-between items-stretch">
-        <div className="content-center text-xl">My Awesome Blog</div>
-        {/* Nav Bar */}
-        <nav className="content-center">
-          <ul className="flex flex-row space-x-10 items-center">
-            <li>
-              <Link className="hover:text-secondary" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:text-secondary" href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <button className="bg-primary text-white text-xs rounded-lg p-3">
-                Random Post
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header className="mx-auto px-16 h-40 py-4 bg-slate-300 flex flex-row justify-between items-stretch">
+      <div className="content-center text-xl">My Awesome Blog</div>
+
+      <nav className="content-center">
+        <ul className="flex flex-row items-center">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                "hover:text-secondary p-1 mr-4 rounded-md " + (isActive ? "active" : "")
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                "hover:text-secondary p-1 mr-12 rounded-md " + (isActive ? "active" : "")
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <button className="bg-primary text-white text-xs rounded-lg p-3">
+              Random Post
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
