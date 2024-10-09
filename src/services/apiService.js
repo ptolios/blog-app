cd4a993cb150a134f795352343fa105ccebe5a88
Panos Tolios <ptolios@deloitte.gr>
2024-10-09 23:27:47 +0300

    Create api call function

---
+++ src/services/apiService.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const api = axios.create({
+  // baseURL: "https://jsonplaceholder.typicode.com/",
+  baseURL: "http://localhost:9000",
+})
+
+function getPosts() {
+  const startParam = Math.floor(Math.random() * 88)
+  const endParam = startParam + 12
+  return api.get("posts", {
+    params: {
+      _start: startParam,
+      _end: endParam,
+    },
+    headers: { "content-type": "application/json" },
+  })
+}
+
+export { getPosts }
