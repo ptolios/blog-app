f7602bc0eb92f660ca5805ae514ef1989f155533
Panos Tolios <ptolios@deloitte.gr>
2024-10-11 17:56:34 +0300

    Handle loading status

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Post from "../components/Post"
 import RelatedPost from "../components/RelatedPost"
 
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(false)
   const [mainPost, setMainPost] = useState({})
   const [firstSecondaryPost, setFirstSecondaryPost] = useState({})
   const [secondSecondaryPost, setSecondSecondaryPost] = useState({})
@@ -13,6 +14,7 @@ function HomePage() {
   useEffect(() => {
     getPosts()
       .then(({ data }) => {
+        setIsLoading(true)
         setMainPost(data?.pop(Math.floor(Math.random() * data.length)))
         setFirstSecondaryPost(data?.pop(Math.floor(Math.random() * data.length)))
         setSecondSecondaryPost(data?.pop(Math.floor(Math.random() * data.length)))
@@ -21,36 +23,44 @@ function HomePage() {
       .catch((err) => {
         console.log(err)
       })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }, [])
 
   return (
     <main className="container mx-auto">
-      {/* Primary Post */}
-      <Post {...mainPost} isMain />
+      {isLoading && <div className="text-5xl text-primary-1 h-screen">Loading...</div>}
+      {!isLoading && (
+        <>
+          {/* Primary Post */}
+          <Post {...mainPost} isMain />
 
-      {/* Secondary Posts */}
-      <div className="flex md:flex-row flex-col gap-8">
-        <Post className="md:w-1/2 w-full" {...firstSecondaryPost} />
-        <Post className="md:w-1/2 w-full" {...secondSecondaryPost} />
-      </div>
+          {/* Secondary Posts */}
+          <div className="flex md:flex-row flex-col gap-8">
+            <Post className="md:w-1/2 w-full" {...firstSecondaryPost} />
+            <Post className="md:w-1/2 w-full" {...secondSecondaryPost} />
+          </div>
 
-      {/* Related Articles*/}
-      <div className="mt-32">
-        <div>
-          <h2 className="text-4xl font-semibold">Related articles or posts</h2>
-        </div>
-        <div className="flex flex-row flex-wrap mx-auto w-full gap-8 justify-between">
-          {relatedPosts.map((post) => (
-            <Link
-              className="mt-8 mb-24 lg:w-1/4 md:w-1/3 w-1/2 grow"
-              to={`/post/${post.id}`}
-              key={post.id}
-            >
-              <RelatedPost key={post.id} post={post} />
-            </Link>
-          ))}
-        </div>
-      </div>
+          {/* Related Articles*/}
+          <div className="mt-32">
+            <div>
+              <h2 className="text-4xl font-semibold">Related articles or posts</h2>
+            </div>
+            <div className="flex flex-row flex-wrap mx-auto w-full gap-8 justify-between">
+              {relatedPosts.map((post) => (
+                <Link
+                  className="mt-8 mb-24 lg:w-1/4 md:w-1/3 w-1/2 grow"
+                  to={`/post/${post.id}`}
+                  key={post.id}
+                >
+                  <RelatedPost key={post.id} post={post} />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </main>
   )
 }
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,16 +3,21 @@ import { useParams } from "react-router-dom"
 import { getPost } from "../services/apiService"
 
 function PostPage() {
+  const [isLoading, setIsLoading] = useState(false)
   const [post, setPost] = useState({})
   const { id } = useParams()
 
   useEffect(() => {
     getPost(id)
       .then(({ data }) => {
+        setIsLoading
         setPost(data)
       })
       .catch((err) => {
         console.log(err)
+      })
+      .finally(() => {
+        setIsLoading(false)
       })
   }, [id])
 
@@ -20,24 +25,32 @@ function PostPage() {
 
   return (
     <div className="mt-36">
+      {isLoading && (
+        <div className="h-screen">
+          <div className="bg-slate-500 text-6xl text-primary-1"></div>
+          Loading post details...
+        </div>
+      )}
       {/* Post contents */}
-      <div className="flex mx-20 gap-36">
-        <div className="w-1/2">
-          <h1 className="text-5xl text-primary-1 mb-6">{post.title}</h1>
+      {!isLoading && (
+        <div className="flex mx-20 gap-36">
+          <div className="w-1/2">
+            <h1 className="text-5xl text-primary-1 mb-6">{post.title}</h1>
 
-          <h2 className="text-2xl text-primary-2 mb-6">{post.subtitle}</h2>
+            <h2 className="text-2xl text-primary-2 mb-6">{post.subtitle}</h2>
 
-          {contents?.map((content) => (
-            <p key={content} className="mb-6">
-              {content}
-            </p>
-          ))}
+            {contents?.map((content) => (
+              <p key={content} className="mb-6">
+                {content}
+              </p>
+            ))}
+          </div>
+          {/* Post image */}
+          <div className="w-1/2">
+            <img className="rounded-xl" src={post.photo?.url} alt={post.photo?.title} />
+          </div>
         </div>
-        {/* Post image */}
-        <div className="w-1/2">
-          <img className="rounded-xl" src={post.photo?.url} alt={post.photo?.title} />
-        </div>
-      </div>
+      )}
     </div>
   )
 }
